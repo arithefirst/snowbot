ac96f2a00179c963b5dec303e667a79eecdbda3c
AriTheFirst <ari@arithefirst.com>
2024-08-18 19:19:41 -0400

    Removed trailing slash in commands/socials/instagram.js for uniformity

--- commands/socials/instagram.js
+++ commands/socials/instagram.js
@@ -4,6 +4,6 @@ module.exports = {
   data: new SlashCommandBuilder().setName("instagram").setDescription("Produces the link to Snow's Instagram"),
 
   async execute(interaction) {
-    await interaction.reply("https://www.instagram.com/iittlesnow/");
+    await interaction.reply("https://www.instagram.com/iittlesnow");
   },
 };
