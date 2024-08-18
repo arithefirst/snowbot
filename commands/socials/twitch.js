1808c01f62f597a878bc670d2b4088868c364664
AriTheFirst <ari@arithefirst.com>
2024-08-18 19:18:29 -0400

    Fixed capitalization in commands/socials/twitch.js

--- commands/socials/twitch.js
+++ commands/socials/twitch.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("twitch").setDescription("Produces the link to Snow's twitch"),
+  data: new SlashCommandBuilder().setName("twitch").setDescription("Produces the link to Snow's Twitch"),
 
   async execute(interaction) {
     await interaction.reply("[ttv/iittlesnow](https://twitch.tv/iittlesnow)");
