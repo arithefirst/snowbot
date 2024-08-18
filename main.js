d77429a8eb95e458e1cd1ae4cc8186d60f616718
AriTheFirst <ari@arithefirst.com>
2024-08-18 15:35:58 -0400

    Moved date2epoch into commands/utility/createTZ.js

--- main.js
+++ main.js
@@ -48,16 +48,4 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 });
 
-function date2epoch(M, D, Y, T, PM) {
-  // If PM is true, add 12 to the time to convert to 24h
-  if (PM == true) {
-    var H24 = +T.split(":")[0] + 12;
-    T = H24 + ":" + T.split(":")[1];
-  }
-
-  // Create the date and return it's epoch value
-  var myDate = new Date(M + " " + D + ", " + Y + " " + T);
-  return myDate.getTime() / 1000.0;
-}
-
 client.login(token);
